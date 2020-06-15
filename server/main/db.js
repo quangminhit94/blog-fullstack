5846db32fded4edb091e666a7f03126cd22cda78
Jimmy in Rain <quangminhit94@gmail.com>
2020-06-16 01:04:11 +0700

    Try another solution

--- server/main/db.js
+++ server/main/db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: 'postgres',
   host: 'dbname3.cz1sxf44hjhp.us-east-2.rds.amazonaws.com',
   database: 'dbname3',
-  password: 'dbname3',
+  password: 'postgres',
   post: 5432,
   ssl: true
 })
