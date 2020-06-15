f39cbd30d99ae2e57c62907f8f4cd177d5e00611
Jimmy in Rain <quangminhit94@gmail.com>
2020-06-16 01:21:46 +0700

    Try a code

--- server/app.js
+++ server/app.js
@@ -15,13 +15,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // if(process.env.NODE_ENV === 'production') {
-if(true) {
-  app.use(express.static('build'))
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+// if(true) {
+//   app.use(express.static('build'))
+//   app.get('/*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
 
-  })
-}
+//   })
+// }
 
 app.use('/', indexRouter)
 
